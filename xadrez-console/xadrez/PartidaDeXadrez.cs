da18823b9bb133689ef042ca387959bc8584b756
lmbsud@gmail.com <lmbsud@gmail.com>
2020-04-11 02:11:47 -0300

    Jogada Especial Promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -145,6 +145,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
 
+            Peca p = Tab.Peca(destino);
+
+            // #jogadaEspecial Promocao
+            if(p is Peao)
+            {
+                if((p.Cor==Cor.Branca && destino.Linha ==0) ||(p.Cor==Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
                 Xeque = true;
@@ -164,7 +180,7 @@ namespace xadrez
                 MudaJogador();
             }
 
-            Peca p = Tab.Peca(destino);
+            
             
             // #jogadaEspecial En Passant
             if(p is Peao && (destino.Linha == origem.Linha -2) || (destino.Linha == origem.Linha + 2))
